4be0d1f4e8afe4ce3b3da65c381a2dd93cb13d17
TheusFL <107876710+theusFL@users.noreply.github.com>
2025-11-05 21:15:10 -0300

    Remove Auth and Upload screens from navigation

--- app/src/main/java/com/example/projeto_mfl_pmovel/navigation/Screen.kt
+++ app/src/main/java/com/example/projeto_mfl_pmovel/navigation/Screen.kt
@@ -1,7 +1,5 @@
 package com.example.projeto_mfl_pmovel.navigation
 
 sealed class Screen(val route: String) {
-    object Auth : Screen("auth_screen")
     object Feed : Screen("feed_screen")
-    object Upload : Screen("upload_screen")
 }
