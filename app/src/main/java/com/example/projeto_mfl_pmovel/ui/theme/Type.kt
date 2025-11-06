233c2be65de2ab0b0386b5692869029c18e0add4
TheusFL <107876710+theusFL@users.noreply.github.com>
2025-11-05 21:24:45 -0300

    Add Typography styles in Type.kt
    Define custom Typography styles for the app.

--- app/src/main/java/com/example/projeto_mfl_pmovel/ui/theme/Type.kt
+++ app/src/main/java/com/example/projeto_mfl_pmovel/ui/theme/Type.kt
@@ -1,4 +1,25 @@
 package com.example.projeto_mfl_pmovel.ui.theme
 
-class Type {
-}
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    )
+ 
+)
