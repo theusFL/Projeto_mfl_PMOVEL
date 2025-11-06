c1a17256f85963bd63821e0a77c74699705e2b0e
TheusFL <107876710+theusFL@users.noreply.github.com>
2025-11-05 21:11:11 -0300

    Update Post data class structure
    Refactor Post data class to include likes and isLiked fields.

--- app/src/main/java/com/example/projeto_mfl_pmovel/data/model/Post.kt
+++ app/src/main/java/com/example/projeto_mfl_pmovel/data/model/Post.kt
@@ -1,16 +1,10 @@
 package com.example.projeto_mfl_pmovel.data.model
 
-// Post.kt (Modificado)
-import com.google.firebase.firestore.ServerTimestamp
-import java.util.Date
-
 data class Post(
-    val id: String = "", // Manteremos o ID do documento aqui
-    val userId: String = "",
-    val username: String = "",
-    val imageUrl: String = "",
-    val description: String = "",
-    val likedBy: List<String> = emptyList(), // Lista de IDs de usuários que curtiram
-    @ServerTimestamp
-    val timestamp: Date? = null
+    val id: String,
+    val username: String,
+    val imageUrl: String,
+    val description: String,
+    val likes: Int,
+    val isLiked: Boolean = false // Para controlar o estado do like localmente
 )
