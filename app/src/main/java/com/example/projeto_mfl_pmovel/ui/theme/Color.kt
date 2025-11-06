ac156d4d0478e1a7d4cfe8c0a80a434df47f5df9
TheusFL <107876710+theusFL@users.noreply.github.com>
2025-11-05 21:27:31 -0300

    Define color palette in Color.kt
    Added a color palette with shades of purple and pink.

--- app/src/main/java/com/example/projeto_mfl_pmovel/ui/theme/Color.kt
+++ app/src/main/java/com/example/projeto_mfl_pmovel/ui/theme/Color.kt
@@ -1,4 +1,12 @@
 package com.example.projeto_mfl_pmovel.ui.theme
 
-class Color {
-}
+import androidx.compose.ui.graphics.Color
+
+// Paleta de exemplo (tons de Roxo/Rosa do M3 padrão)
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
