0f8377530c60204d9be79b3585c0152e05721fb7
TheusFL <107876710+theusFL@users.noreply.github.com>
2025-11-12 15:23:42 -0300

    Change start destination to Login screen

--- app/src/main/java/com/example/projeto_mfl_pmovel/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/projeto_mfl_pmovel/navigation/AppNavigation.kt
@@ -4,17 +4,28 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.projeto_mfl_pmovel.ui.auth.LoginScreen 
 import com.example.projeto_mfl_pmovel.ui.feed.FeedScreen
 
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
-
-    val startDestination = Screen.Feed.route
+    val startDestination = Screen.Login.route
 
     NavHost(navController = navController, startDestination = startDestination) {
 
-        // Tela de Feed (única tela)
+        composable(Screen.Login.route) {
+            LoginScreen(
+                onLoginClick = {
+                    navController.navigate(Screen.Feed.route) {
+                        popUpTo(Screen.Login.route) {
+                            inclusive = true
+                        }
+                    }
+                }
+            )
+        }
+
         composable(Screen.Feed.route) {
             FeedScreen()
         }
