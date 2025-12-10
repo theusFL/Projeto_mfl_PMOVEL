2ade6a0252609f0f2efb59a64f4c9b4700a67579
TheusFL <107876710+theusFL@users.noreply.github.com>
2025-12-10 16:37:21 -0300

    Integrate PostRepository into MainActivity

--- app/src/main/java/com/example/projeto_mfl_pmovel/MainActivity.kt
+++ app/src/main/java/com/example/projeto_mfl_pmovel/MainActivity.kt
@@ -4,16 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.projeto_mfl_pmovel.ui.theme.Projeto_mfl_PMOVELTheme
+import androidx.room.Room
+import com.example.projeto_mfl_pmovel.data.local.AppDatabase
+import com.example.projeto_mfl_pmovel.data.remote.PostRemoteDataSource
+import com.example.projeto_mfl_pmovel.data.repository.PostRepository
 import com.example.projeto_mfl_pmovel.navigation.AppNavigation
+import com.example.projeto_mfl_pmovel.ui.theme.Projeto_mfl_PMOVELTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val db = Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "app-database"
+        ).build()
+
+        val remoteDataSource = PostRemoteDataSource()
+        val repository = PostRepository(db.postDao(), remoteDataSource)
+
         setContent {
             Projeto_mfl_PMOVELTheme {
-                AppNavigation()
+                AppNavigation(repository)
             }
         }
     }
